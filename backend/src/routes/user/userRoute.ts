2c25312537111e19c6381a075401d48319245d73
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2026-01-04 09:34:32 +0700

    test: test

--- backend/src/routes/user/userRoute.ts
+++ backend/src/routes/user/userRoute.ts
@@ -6,5 +6,6 @@ const route = Router();
 route.post('/auth/register', userController.register);
 route.post('/auth/login', userController.login);
 route.post('/auth/logout', userController.logout);
+route.get('/', userController.logout);
 
 export default route;
